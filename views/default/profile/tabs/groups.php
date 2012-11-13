8295b90745b56b3ab26e8d1fcc76d14734f99142
Jeff Tilson <jtilson@thinkglobalschool.com>
2012-11-13 12:10:54 -0500

    Passing limit and offset into listEntities for profile groups list

--- views/default/profile/tabs/groups.php
+++ views/default/profile/tabs/groups.php
@@ -3,7 +3,7 @@
  * Profile groups
  */
 
-$groups = $vars['entity']->listGroups();
+$groups = $vars['entity']->listGroups(NULL, get_input('limit', 10), get_input('offset'));
 
 if (!$groups) {
 	$groups = '<p>' . elgg_echo('profile:no_groups') . '</p>';
